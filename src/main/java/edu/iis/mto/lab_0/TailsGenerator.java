5fccd034a67e79c0e2ba38dcc171d55a471e2571
Wojtczak.Adam <215922@edu.p.lodz.pl>
2020-03-03 18:34:53 +0100

    lab0 poprawa listy
    Signed-off-by: Wojtczak.Adam <215922@edu.p.lodz.pl>

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        ArrayList tailsArray = new ArrayList<>(value.length());
+        List tailsList = new ArrayList<>(value.length());
         for(int i = 0; i < value.length()+1; i++) {
-            tailsArray.add(value.subSequence(i,value.length()));
+            tailsList.add(value.subSequence(i,value.length()));
         }
-        return tailsArray;
+        return tailsList;
     }
 }
